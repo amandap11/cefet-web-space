968e7ba2ee4c1b7485ffa6a08b854f2135299f17
Amanda <afp1103@gmail.com>
2017-04-11 09:27:30 -0300

    exercicio1

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,24 @@
 // Faça o exercício dos parágrafos aqui
 // Este arquivo ainda não está incluído no arquivo HTML
+
+let buttons = document.getElementsByClassName("botao-expandir-retrair");
+let paragraphs = document.getElementsByTagName("p");
+
+for (let i = 0; i < buttons.length; i++){
+
+	buttons[i].addEventListener('click', function (ev){
+
+		if (buttons[i].innerHTML == "+"){
+			paragraphs[i].classList.add('expandido');
+			buttons[i].innerHTML = "-";
+		} else {
+			paragraphs[i].classList.remove('expandido');
+			buttons[i].innerHTML = "+";
+		}
+		
+	});
+
+}
+
+
+
