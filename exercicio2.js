1eb123bc99446937a6e1007927a262588812378d
Amanda <afp1103@gmail.com>
2017-04-11 10:21:33 -0300

    inicio exercicio2

--- exercicio2.js
+++ exercicio2.js
@@ -9,3 +9,26 @@ const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
     'philae-67-picture.jpg',
     'philae-collecting.jpg'
   ];
+
+let imagem = document.querySelector("#galeria img");
+let botaoProximo = document.getElementById("proximo");
+let botaoAnterior = document.getElementById("anterior");
+
+botaoProximo.addEventListener('click', function (ev){
+
+	let primeiraImagem = servidorDasImagens + todasAsImagens[0];
+	let segundaImagem = servidorDasImagens + todasAsImagens[1];
+	let terceiraImagem = servidorDasImagens + todasAsImagens[2];
+	let quartaImagem = servidorDasImagens + todasAsImagens[3];
+	let quintaImagem = servidorDasImagens + todasAsImagens[4];
+
+	let caminhoAtual = imagem.src;
+
+	if (caminhoAtual == primeiraImagem){
+		imagem.src = segundaImagem;
+	} 
+});
+
+botaoAnterior.addEventListener('click', function (ev){
+	
+});
